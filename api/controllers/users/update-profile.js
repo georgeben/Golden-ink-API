e20c3a8265f6d89684800aeb340e2619edc95a2f
George Benjamin <msdcconnect@gmail.com>
2020-03-04 15:43:43 +0100

    implement image upload in update profile

--- api/controllers/users/update-profile.js
+++ api/controllers/users/update-profile.js
@@ -1,44 +1,30 @@
-module.exports = {
 
-
-  friendlyName: 'Update profile',
-
-
-  description: 'Updates a users profile',
-
-
-  inputs: {
-    name: {
-      type: 'string',
+module.exports = function(req, res) {
+  const user = req.user;
+  const requestData = req.body;
+  //Check for updated profile photo
+  req.file('profilePhoto').upload(
+    {
+      maxBytes: 5120000,
+      adapter: require('skipper-s3'),
+      key: sails.config.aws.accessKey,
+      secret: sails.config.aws.secretAccessKey,
+      bucket: 'golden-ink-user-photos'
     },
-    username: {
-      type: 'string',
-    },
-    headline: {
-      type: 'string',
-    },
-    bio: {
-      type: 'string'
+    async function uploadComplete(err, uploadedFiles) {
+      if (err) {
+        return res.serverError();
+      }
+      if (uploadedFiles.length > 0) {
+        const profilePhotoUrl = `${sails.config.aws.s3baseUrl}/${uploadedFiles[0].fd}`;
+        requestData.profilePhotoUrl = profilePhotoUrl;
+      }
+      const updatedUser = await Users.updateOne({
+        id: user.id
+      }).set(requestData);
+      return res.json({
+        data: updatedUser
+      });
     }
-  },
-
-
-  exits: {
-
-  },
-
-
-  fn: async function (inputs) {
-    // TODO Implement update profile photo
-    const user = this.req.user;
-    const updatedUser = await Users.updateOne({
-      id: user.id,
-    }).set(inputs);
-    return {
-      data: updatedUser,
-    };
-
-  }
-
-
+  );
 };
